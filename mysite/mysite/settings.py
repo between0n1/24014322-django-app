78a4bb4f4bbebb8a5eda555f5fa001770ec52894
between0n1 <sunhyeok.jung22@myhunter.cuny.edu>
2022-09-11 21:35:40 -0400

    ASD

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 
 
 # Enable WhiteNoise's GZip compression of static assets.
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
